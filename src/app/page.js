6977e92124240c74c056d64d35cc6fecde9dbd8e
Arkadiusz Cios <arkadiuszcios@outlook.com>
2025-05-14 16:16:38 +0200

    feat: #dev update question page with real questions

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,17 @@
 import Image from "next/image";
 import Question from "../app/ui/MBTIquestion.jsx"
+import TestQuestions from "../../data/TestQuestions.js";
 
-const sampleQuestion = {
-  question: "Jaki jest największy ocean na świecie?",
-  answers: [
-    "Ocean Atlantycki",
-    "Ocean Spokojny",
-    "Ocean Indyjski",
-    "Ocean Arktyczny"
-  ]
-};
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header> test </header>
-      <main>
-        <Question questionData={sampleQuestion} />
+      <main className="flex flex-col gap-10">
+        {TestQuestions.map((questionObj, index) => (
+          <Question key={index} questionData={questionObj} />
+        ))}
       </main>
-      
     </div>
   );
 }
--- src/app/ui/MBTIquestion.jsx
+++ src/app/ui/MBTIquestion.jsx
@@ -1,18 +1,24 @@
 export default function Question({ questionData }) {
-    const { question, answers } = questionData;
-  
-    return (
-      <div className="p-4 border rounded-xl shadow-md bg-black max-w-xl mx-auto">
-        <h2 className="text-xl font-semibold mb-4">{question}</h2>
-        <ul className="space-y-2">
-          {answers.map((answer, index) => (
-            <li key={index}>
-              <button className="w-full text-left p-2 bg-gray-700 hover:bg-gray-900 rounded">
-                {answer}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+  const {id, question, answers } = questionData;
+
+  return (
+    <div className="p-4 border rounded-xl shadow-md bg-black max-w-xl">
+      <h2 className="text-xl font-semibold mb-4">{question}</h2>
+      <ul className="space-y-2">
+        {Object.keys(answers).map((key) => {
+          const answer = answers[key];
+          return (
+            <div>
+              <li key={key}> 
+                <div class="flex items-center ps-4 border border-gray-200 rounded-sm dark:border-gray-700">
+                    <input type="radio" value={answer.text} name={id} class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                    <label for={answer.text} class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{answer.text}</label>
+                </div>
+              </li>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
