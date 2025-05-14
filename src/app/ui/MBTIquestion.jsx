4d64a75591a3694db87f4da1164b5745df0e11dd
Arkadiusz Cios <arkadiuszcios@outlook.com>
2025-05-14 16:21:38 +0200

    feat: #dev make radio clickable

--- src/app/ui/MBTIquestion.jsx
+++ src/app/ui/MBTIquestion.jsx
@@ -8,14 +8,12 @@ export default function Question({ questionData }) {
         {Object.keys(answers).map((key) => {
           const answer = answers[key];
           return (
-            <div>
               <li key={key}> 
                 <div class="flex items-center ps-4 border border-gray-200 rounded-sm dark:border-gray-700">
-                    <input type="radio" value={answer.text} name={id} class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                    <input id={answer.text} type="radio" value={answer.text} name={id} class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
                     <label for={answer.text} class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{answer.text}</label>
                 </div>
               </li>
-            </div>
           );
         })}
       </ul>
