bd0accb742da0fbe5726f76181175ce78f04c2e0
Arkadiusz Cios <arkadiuszcios@outlook.com>
2025-05-29 11:13:18 +0200

    reactor: #13 move MBTI component to components dir

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import Question from "../ui/MBTIquestion";
+import Question from "../../components/MBTIquestion";
 import TestQuestions from "../../data/TestQuestions";
 import { useRouter } from "next/navigation";
 
